1a90693f76feb1f1105e43e5148401ce09b0d74d
tomkuenzler <tkotter24@gmail.com>
2016-03-20 04:17:34 -0400

    App Availability #3

--- www/js/appAvailability.js
+++ www/js/appAvailability.js
@@ -2,7 +2,7 @@ function openFacebook() {
 	appAvailability.check(
     'fb://', // URI Scheme
     function() {  // Success callback
-        window.open('fb://profile/335761773297446', '_system', 'location=yes');
+        window.open('https://www.facebook.com/335761773297446','_system','location=yes');
     },
     function() {  // Error callback
         window.open('https://www.facebook.com/335761773297446','_system','location=yes');
