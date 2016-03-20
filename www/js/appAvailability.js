1d1605bc4a7306d508360c88a88cd5814fb23060
tomkuenzler <tkotter24@gmail.com>
2016-03-20 03:04:33 -0400

    App Availability #2

--- www/js/appAvailability.js
+++ www/js/appAvailability.js
@@ -1,6 +1,15 @@
-function openFB() {
-	appAvailability.check('fb://', function(availability) {
-		if(availability) { window.open('fb://profile/335761773297446', '_system', 'location=yes');}
-		else{window.open('https://www.facebook.com/335761773297446','_system','location=yes');}
-	});
+function openFacebook() {
+	appAvailability.check(
+    'fb://', // URI Scheme
+    function() {  // Success callback
+        window.open('fb://profile/335761773297446', '_system', 'location=yes');
+    },
+    function() {  // Error callback
+        window.open('https://www.facebook.com/335761773297446','_system','location=yes');
+    }
+	);
 }
+
+// function openFacebook() {
+// 	window.open('https://www.facebook.com/335761773297446','_system','location=yes');
+// }
