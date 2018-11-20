c4c0a6800558fa51f46f1ffb023583d5dd3c3f5f
tomkuenzler <tkotter24@gmail.com>
2018-11-20 05:36:26 -0500

    InApp Browser Improvements

--- www/js/appAvailability.js
+++ www/js/appAvailability.js
@@ -5,7 +5,7 @@ function openFacebook() {
 	        window.open('fb://profile/335761773297446', '_system');
 	    },
 	    function() {  // Error callback
-	        cordova.InAppBrowser.open('https://www.facebook.com/335761773297446','_blank');
+	        cordova.InAppBrowser.open('https://www.facebook.com/335761773297446','_blank','location=no,closebuttoncaption=Back');
 	    }
 	);
 }
@@ -17,7 +17,7 @@ function openInstagram() {
 	        window.open('instagram://user?username=joeys_pizzapasta', '_system');
 	    },
 	    function() {  // Error callback
-	        cordova.InAppBrowser.open('https://www.instagram.com/joeys_pizzapasta/','_blank');
+	        cordova.InAppBrowser.open('https://www.instagram.com/joeys_pizzapasta/','_blank','location=no,closebuttoncaption=Back');
 	    }
 	);
 }
